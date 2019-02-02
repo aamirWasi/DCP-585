cd8384f47e32fa7f3ec5c617bbd6727ed0802187
aamir <wasi3aamir@gmail.com>
2019-02-02 23:49:18 +0600

    full time wasted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            //string value = "Welcome To Csharpstar";
+            // Use ToCharArray to convert string to array.
+            //char[] array = str1.ToCharArray();
+            int count = 0;
+            // Loop through array.
+            for (int i = 0; i < str1.Length; i++)
+            {
+                // Get character from array.
+                char ch = str1[i];
+                if (ch==str1[i])
+                {
+                    count++;
+                    Console.WriteLine(ch);
+                }
+            }
+        }
+    }
+}
